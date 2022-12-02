d4d50c5d0732818519d120c104fc69cf4a3289fa
aberger3647 <112574259+aberger3647@users.noreply.github.com>
2022-12-02 13:29:07 -0600

    return saved notes as json, save new notes, and give each note a unique id

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { readFile, writeFile } = fs.promises;
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -20,6 +23,28 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+// Read db.json file and return saved notes as JSON
+app.get('/api/notes', (req, res) => {
+  readFile('db/db.json')
+  .then(data => {
+    res.send(data)
+  })
+});
+
+// Create and save new note
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  readFile('db/db.json')
+  .then(data => {
+    const allNotes = JSON.parse(data);
+    allNotes.push(newNote);
+    writeFile('db/db.json', JSON.stringify(allNotes)).then(data => {
+      res.json(newNote);
+    })
+  })
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
